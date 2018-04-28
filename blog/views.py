9e5fb85b8f3c78c2a3a7a92e34554fc56cfd5f98
ChangeTheWorld <4903000@gmail.com>
2018-04-28 09:34:09 +0800

    pagination

--- blog/views.py
+++ blog/views.py
@@ -17,6 +17,54 @@ class IndexView(ListView):
     context_object_name = 'post_list'
     #指定 paginate_by 属性后开启分页功能，其值代表每一页包含多少篇文章
     paginate_by = 2
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        paginator = context.get('paginator')
+        page = context.get('page_obj')
+        is_paginated = context.get('is_paginated')
+        pagination_data = self.pagination_data(paginator, page, is_paginated)
+        context.update(pagination_data)
+        return context
+        
+    def pagination_data(self, paginator, page, is_paginated):
+        if not is_paginated:
+            return {}
+        left = []
+        right = []
+        left_has_more = False    
+        right_has_more = False    
+        first = False    
+        last = False   
+ 
+        page_number = page.number
+        total_pages = paginator.num_pages
+        page_range = paginator.page_range
+
+        left = page_range[(page_number - 3) if (page_number - 3) > 0 else 0:
+                          (page_number - 1) if (page_number - 1) > 0 else 0]
+        right = page_range[page_number:page_number + 2]
+
+        if right:
+            if right[-1] < total_pages:
+                last = True        
+            if right[-1] < total_pages - 1:
+                right_has_more = True    
+        if left:
+            if left[0] > 1:
+                first = True        
+            if left[0] > 2:
+                left_has_more = True  
+  
+        data = {'left': left,
+                'right': right,        
+                'left_has_more': left_has_more,
+                'right_has_more': right_has_more, 
+                'first': first,       
+                'last': last,}
+        return data
+        
+        
 '''    
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
