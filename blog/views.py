2689f781404d5e3e7b44b0fd101f1d6c7340215d
THINK <4903000@gmail.com>
2018-04-29 22:57:22 +0800

    tag

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 '''from django.http import HttpResponse'''
-from .models import Post, Category
+from .models import Post, Category, Tag
 import markdown
 from comments.forms import CommentForm
 from django.views.generic import ListView, DetailView
@@ -140,5 +140,12 @@ class ArchivesView(IndexView):
         return super(ArchivesView, self).get_queryset().filter(created_time__year=year,
                                                                created_time__month=month
                                                                )
-        
-    
+															   
+class TagView(ListView):
+	model = Post
+	template_name = 'blog/index.html'
+	context_object_name = 'post_list'
+	
+	def get_queryset(self):
+		tag = get_object_or_404(Tag, pk=self.kwargs.get('pk'))
+		return super(TagView, self).get_queryset().filter(tags=tag)
